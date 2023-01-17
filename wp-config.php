953a2e1a6b014b98ce7ea00676e8935b45d8b6c8
Anzhelika <anzhelika.flyorko@gmail.com>
2023-01-17 03:57:19 +0300

    загрузила сборку WP

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'belsono' );
+define( 'DB_NAME', 'u257925500_belsono' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u257925500_belsono' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'BelSono2023' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
